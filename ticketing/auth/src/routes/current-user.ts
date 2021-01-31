8ed2e254d6d027492c7dbf3dc25f66df121f0fe1
mistyjack <oladepomustafa@gmail.com>
2021-01-31 18:30:17 +0100

    Added auth routes tests.

--- ticketing/auth/src/routes/current-user.ts
+++ ticketing/auth/src/routes/current-user.ts
@@ -5,7 +5,7 @@ import { currentUser } from "../middlewares/current-user";
 const router = express.Router();
 
 router.get("/api/users/currentuser", currentUser, (req, res) => {
-  res.send({ currentuser: req.currentUser || null });
+  res.send({ currentUser: req.currentUser || null });
 });
 
 export { router as currentUserRouter };
